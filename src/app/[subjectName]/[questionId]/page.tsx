9667bcdd7a9ed2476d145964058a15deb9423df3
elvin2019 <elvinahmadov0@gmail.com>
2024-01-07 19:48:58 +0400

    adding envorinment variable for server url

--- src/app/[subjectName]/[questionId]/page.tsx
+++ src/app/[subjectName]/[questionId]/page.tsx
@@ -1,6 +1,6 @@
 import QuestionComponent from "./question";
 const Questions = async () => {
-   const questions = await fetch('http://localhost:3000/api/questions', {method: 'GET', cache: 'force-cache'}).then((res) => res.json());
+   const questions = await fetch(process.env.SITE_URL + '/api/questions', {method: 'GET', cache: 'force-cache'}).then((res) => res.json());
    return <QuestionComponent questions={questions}/>
 } 
 
